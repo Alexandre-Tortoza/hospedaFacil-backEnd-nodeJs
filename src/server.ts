036b43dd99754275bce17c378f0c7192d80b4763
Alexandre Tortoza <85268093+Alexandre-Tortoza@users.noreply.github.com>
2024-12-22 12:19:41 -0300

    [fix] - port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
 const startServer = async () => {
-  app.listen({ port: 3333 }).then(() => {
+  app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
     console.log(
       "Server is running on port 3333 \n\nSwagger: http://localhost:3333/docs "
     );
